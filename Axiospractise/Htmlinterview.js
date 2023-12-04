d989bdd20006ea3bac917ee563253d3f86cf1484
lavanyayaleru <152532450+lavanyayaleru@users.noreply.github.com>
2023-12-04 18:04:06 +0530

    Add files via upload

---
+++ Axiospractise/Htmlinterview.js
@@ -0,0 +1,52 @@
+// Explain the difference between HTML and HTML5.
+
+// *HTML is the standard markup language for creating web pages, while HTML5 is its latest version, offering new features like 
+//  Multimedia tags & Semantic tags compatibility with modern web technologies.
+// ====================================================================================================================================================
+
+// What are the new features introduced in HTML5?
+
+// *HTML5 introduced new features like Multi-media tags as audio and video playback, Semantic tags as <section>,<main>,<header>,<footer>,<nav> and better
+//  compatibility with mobile devices.
+// =======================================================================================================================================================
+
+// How do you embed audio and video in HTML?
+
+// *To embed audio, use <audio> with the src attribute for the audio file. For video, use <video> with the src attribute for the video file.
+// *Ex:- <audio src="audio.mp3" controls></audio>, Ex:-<video src="video.mp4" controls></video>.
+// ======================================================================================================================================================
+
+// What is the purpose of the <!DOCTYPE> declaration?
+
+// *The <!DOCTYPE> declaration is the very first line of an html document & informs the browser about the version of an html being used.
+// ======================================================================================================================================================
+
+// Explain the difference between <div> and <span> elements.
+
+// *<div> is a block-level element used for grouping and structuring content, while 
+// *<span> is an inline element used for applying styles or scripting to small pieces of text within a larger block of content.
+// ======================================================================================================================================================
+
+// How do you create a hyperlink in HTML?
+
+// *Use the <a> (anchor) element with the href attribute to create a hyperlink.
+// *For example: <a href="https://example.com">Click here</a>.
+// ======================================================================================================================================================
+
+// What is the purpose of the alt attribute in the <img> tag?
+
+// *The "alt" attribute in the <img> tag provides alternative text describing the image.
+// ======================================================================================================================================================
+
+// What is semantic HTML, and why is it important?
+
+// *Semantic tags are also called as Html tags which adds the meaningful name to the tag also,it helps the browsers and developers understand the document's Organization.
+// =========================================================================================================================================================
+
+// what are attributes in html?
+
+// *In Html attributes is a Piece of markup language which is used to adjust the behaviour of an html element.
+// Attributes are inserted in the opening tags of the elements and have a name-Value pair.
+// name is used to specified the property.
+// value is defined the value of the property.
+// Ex: <a>,<href>,src,<img>.
